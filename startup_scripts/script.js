977aec67ac19e0742bb82264c144f92b6e7b8418
Yuri Yaryshev <yuyaryshev@gmail.com>
2024-08-23 21:38:27 +0300

    - Long ago forgotten update in work folder update... Maybe not needed

--- startup_scripts/script.js
+++ startup_scripts/script.js
@@ -3,7 +3,7 @@
 
 // console.info('Hello, World! (You will only see this line once in console, during startup)')
 // kubejs/assets/kubejs/textures/item/test_item.png
-	
+
 // onEvent('item.registry', event => {
 // 	// Register new items here
 // 	// event.create('example_item').displayName('Example Item')
@@ -14,32 +14,45 @@
 // 	// event.create('example_block').material('wood').hardness(1.0).displayName('Example Block')
 // })
 
-onEvent('item.modification', event => {
-	
-	const stackSizes = {
-		'minecraft:raw_iron':2,
-		'minecraft:raw_gold':2,
-		'create:raw_zinc':2,
-		'minecraft:raw_copper':2,
-// 'occultism:raw_silver':2, // Нужно посмотреть в игре верный id выпадающего предмета из этой руды
-		'tconstruct:raw_cobalt':2,
-		// mekanism:osmium_ore
-		// mekanism:tin_ore
-		// mekanism:lead_ore
-	};
-	
-	for(const k in stackSizes) {
-	  event.modify(k, item => {
-		item.maxStackSize = stackSizes[k];
-	  });
-	}
+onEvent("item.modification", (event) => {
+    const stackSizes = {
+        "minecraft:raw_iron": 2,
+        "minecraft:raw_gold": 2,
+        "create:raw_zinc": 2,
+        "minecraft:raw_copper": 2,
+        // 'occultism:raw_silver':2, // Нужно посмотреть в игре верный id выпадающего предмета из этой руды
+        "tconstruct:raw_cobalt": 2,
+        // mekanism:osmium_ore
+        // mekanism:tin_ore
+        // mekanism:lead_ore
+    };
 
-	event.modify('minecraft:iron_sword',item => {
-		item.maxDamage = 333; // Изменение максимальной прочности предмета.
-		item.attackDamage = 444;
-		item.setAttackDamage(555);
-	});
-})
+    for (const k in stackSizes) {
+        event.modify(k, (item) => {
+            item.maxStackSize = stackSizes[k];
+        });
+    }
+
+    event.modify("minecraft:iron_sword", (item) => {
+        item.maxDamage = 333; // Изменение максимальной прочности предмета.
+        item.attackDamage = 444;
+        item.setAttackDamage(555);
+        const t = (tierOptions) => {
+            // int uses
+            // float speed
+            tierOptions.attackDamageBonus = 777;
+            // int level
+            // int enchantmentValue
+			tierOptions.repairIngredient = 'minecraft:copper_ingot';
+            // Ingredient repairIngredient
+        };
+		t.attackDamageBonus = 77;
+		t.repairIngredient = 'minecraft:copper_nugget';
+
+        item.tier = t;
+        item.tierOptions = t;
+    });
+});
 
 // onEvent('worldgen.add', event => {
 //   event.addOre(ore => {
